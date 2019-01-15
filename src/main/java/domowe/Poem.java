1f5f5977ac59416b873ee0be2413de8c06fb01f4
abramovvicz <abramovvicz@gmail.com>
2019-01-15 20:34:24 +0100

    poprawa obsługi pliku i zapisywanie treści do plliku

--- src/main/java/domowe/Poem.java
+++ src/main/java/domowe/Poem.java
@@ -1,6 +1,7 @@
 package domowe;
 
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,25 +10,42 @@ import java.util.stream.Stream;
 
 public class Poem {
 
+    private Stream<String> readFile = Files.lines(Paths.get("brzechwa.txt"));
+    private String text = "";
+
+    public Poem() throws IOException {
+    }
 
     public static void main(String[] args) throws IOException {
         Poem poem = new Poem();
         poem.transformFile();
+        poem.writeFile();
     }
 
-    private void transformFile() throws IOException {
-        Stream<String> readFile = Files.lines(Paths.get("brzechwa.txt"));
+    private void transformFile() {
         /*someString
                 .map(x->x.substring(0,1).toUpperCase()+x.substring(1))
                 .map(x->x+",")
                 .forEach(System.out::println);*/   // tutaj przecinek zostawał na końcu wiersza
 
-        String text = readFile
+        text = readFile
                 .map(x -> x.substring(0, 1).toUpperCase() + x.substring(1))
                 .collect(Collectors.joining(",\n"));
 
         System.out.println(text);
 
-        Path writeFile = Files.createFile(Paths.get("modified.txt"));
     }
+
+    private void writeFile() throws IOException {
+        try {
+            Path path = Files.createFile(Paths.get("modified.txt")); // tworzenie pliku
+            Path files = Files.write(path, text.getBytes()); // zapisywanie treści do pliku
+        } catch (FileAlreadyExistsException e) {
+            Files.delete(Paths.get("modified.txt"));
+            Path path = Files.createFile(Paths.get("modified.txt"));
+            Path files = Files.write(path, text.getBytes());
+
+        }
+    }
+
 }
