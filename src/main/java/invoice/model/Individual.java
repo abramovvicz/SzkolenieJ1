d6d5dfae4a0be17f944f94e3162037c59dc0587b
abramovvicz <abramovvicz@gmail.com>
2019-01-27 13:31:27 +0100

    pojscie na innego brancha

--- src/main/java/invoice/model/Individual.java
+++ src/main/java/invoice/model/Individual.java
@@ -1,4 +1,12 @@
 package invoice.model;
 
-public class Individual {
+public class Individual extends Client {
+    public Individual(int id, ClientType clientType, String shortName, String name, Adress adress, String nip, String pesel) {
+        super(id, clientType, shortName, name, adress, nip, pesel);
+    }
+
+
+    public  String getPesel(){
+        
+    }
 }
